92d5469f21cfcf8980170300b6f4e22d43233896
DanieRae <daniellembmacrae@gmail.com>
2022-01-24 12:43:25 -0500

    New script

---
+++ 3-stats.R
@@ -0,0 +1,6 @@
+#Statistics and response variables#
+
+# ga.fish.traits.dist <- gawdis(fish.traits)
+
+functional.diversity.FT <- dbFD(fish.traits.dist,
+                                corr = "lingoes")
