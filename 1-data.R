9fed09a9e17eed991f6d4ece36feaba7016a2810
DanieRae <daniellembmacrae@gmail.com>
2022-02-01 09:58:12 -0500

    Added new data set

--- 1-data.R
+++ 1-data.R
@@ -18,7 +18,7 @@ library(data.table)
 library(vegan)
 library(gclus)
 
-
+#PART ONE#
 #load full data and view it#
 
 atlantic.fish.traits <- read.csv("AtlanticFishTraits.csv", fileEncoding ="UTF-8-BOM", stringsAsFactors = TRUE)
@@ -41,7 +41,7 @@ new_rownames <- unlist(lapply(lapply(str_split(old_rownames, " "),
 new_rownames[new_rownames == "Myo_sco"] <- c("Myo_sco_1", "Myo_sco_2")
 rownames(atlantic.fish.traits) <- new_rownames
  
-norm <- decostand(atlantic.fish.traits, "normalize")
+norm <- decostand(atlantic.fish.traits, "normalize") #DOES THIS MATTER?#
 
 #quick mutate to remove negative values in these columns#
 atlantic.fish.traits <- atlantic.fish.traits %>%
@@ -55,3 +55,34 @@ fish.traits <- select(atlantic.fish.traits, -c ("Genus","Subfamily", "Family", "
 
 #View current data format#
 View(fish.traits)
+
+
+#PART TWO#
+#Load abundance data#
+
+fish.abun <- read.csv("NL_Biomass.csv", fileEncoding ="UTF-8-BOM")
+View(fish.abun)
+
+#I need to make the taxa names match with the other data set
+
+#This takes the species list and puts it into a character#
+species_name <- fish.abun$taxa_name
+
+#We are taking the character to then change the species names from all CAPS to just having the first letter in CAP, returns better data#
+species_name <- str_to_sentence(species_name)
+
+#Shortening the names to math
+species_name[species_name == "Myoxocephalus scorpioides"] <- c("Myoxocephalus scorpioides 1")
+species_name[species_name == "Myoxocephalus scorpius"] <- c("Myoxocephalus scorpius 2")
+
+new_species_name <- unlist(lapply(lapply(str_split(species_name, " "), 
+                                     FUN = str_sub, 1, 3), paste0, collapse = "_"))
+
+fish.abun <- select(fish.abun, -c ("taxa_name"))
+
+fish.abun <- cbind(fish.abun, taxa_name = new_species_name)
+
+#Need to cut out all the species not included in the main data
+
+
+df1 <- fish.abun %>% filter(taxa_name == new_rownames)
