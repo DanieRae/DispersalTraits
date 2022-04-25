8b509c343a4d0258a248fa731128069cb1414269
Valentin <valentin.lucet@gmail.com>
2022-04-25 13:14:23 -0400

    add beta div calculation and plotting

--- 3-maps.R
+++ 3-maps.R
@@ -69,14 +69,103 @@ effective.FD <-fish.abun.clust %>%
   dplyr::summarize(effective_species = exp(diversity(group_biomass, "shannon")))
 
 
-#Effective species Diversity with Shannnon on species                       
-effective.species <-fish.abun.clean %>%
+#Effective species Diversity with Shannnon on species     
+# We first summarize the biomass data to get the man biomass for each year 
+# and stratum
+fish.abun.clean.summary <- fish.abun.clean %>%
   #first find, for each trawl and each functional group, the total biomass of that group in that trawl
   group_by(stratum, year_surv, vessel, trip, set, taxa_name)%>%
   dplyr::summarize(group_biomass = sum(density_kgperkm2))%>%
   #calculate average biomass per stratum for each functional group
-  group_by(stratum, year_surv, taxa_name)%>%
-  dplyr::summarize(group_biomass = mean(group_biomass))%>%
+  group_by(stratum, year_surv, taxa_name) %>%
+  dplyr::summarize(group_biomass = mean(group_biomass)) %>% 
+  ungroup()
+
+# BETA
+# We split our main dataframe into a list of each dataframe corresponding to 
+# each stratum
+fish.abun.clean.summary.split <- fish.abun.clean.summary %>% 
+  # We exclude stratum for which there is only data for 1 year
+  filter(!(stratum %in% 
+             c(501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511,
+               512, 513, 514, 515, 516, 517, 518, 519, 710, 915, 917, 918))) %>%
+  # Split things
+  split.data.frame(.$stratum)
+
+# Using lapply, we apply a function to each data frame in the list. Here we 
+# create a presence absence dataframe for each species and year/stratum 
+# combinations, by pivoting the taxa column into columns
+fish.pa.pivot <- 
+  lapply(fish.abun.clean.summary.split, 
+         function(df){
+           df <- df %>% 
+             mutate(presence_absence = as.numeric(group_biomass > 0), 
+                    stratum_year = str_c(stratum, "_", year_surv)) %>% 
+             pivot_wider(names_from = taxa_name, values_from = presence_absence, 
+                         id_cols = stratum_year)
+         }
+  )
+
+# Still with lapply, we turn our dataframes into matrices and make sure their 
+# rownames correspond to the stratum/year combination
+fish.pa.pivot.mat <- lapply(fish.pa.pivot, function(df) {
+  # remove first column to be casted correctly as integer
+  mat <- as.matrix(df[,-1])
+  mat[is.na(mat)] <- 0
+  rownames(mat) <- df$stratum_year
+  return(mat)
+}
+)
+
+# Still with lapply, we compute the beta diversity (see ?betadiver), cast as a 
+# matrix and return
+fish.beta <- lapply(fish.pa.pivot.mat, function(mat) {
+  mat <- as.matrix(betadiver(mat, 4))
+  return(mat)
+}
+)
+
+# Still with lapply, we turn our matrix back into a usable, joinable dataframe
+# - we first collect the rownames, pivot, rename, separate the columns and 
+# wrangle the data so that the "to" year is later than the "from" year
+fish.beta.df <- lapply(fish.beta, function(mat) {
+  as.data.frame(mat) %>% 
+    tibble::rownames_to_column() %>% 
+    pivot_longer(cols = 2:ncol(.)) %>% 
+    unique() %>% 
+    dplyr::rename(from = rowname, to = name) %>% 
+    tidyr::separate(from, c("from_stratum", "from_year"), "_") %>% 
+    tidyr::separate(to, c("to_stratum", "to_year"), "_") %>% 
+    select(-to_stratum) %>% 
+    dplyr::rename(stratum = from_stratum) %>%
+    filter(to_year > from_year)
+})
+
+# We turn the list into a fully combined dataframe
+fish.beta.df.full <- bind_rows(fish.beta.df)
+
+# We join to the stratum spatial data
+fish.beta.df.full.joined <- fish.beta.df.full %>% 
+  left_join(stratum.new, by = "stratum") %>% 
+  st_as_sf()
+
+# We select the years and plot
+beta.map <- fish.beta.df.full.joined %>% 
+  filter(from_year == 2000, 
+         to_year == 2005) %>% 
+  ggplot() +
+  geom_sf(aes(fill = value)) +
+  ggtitle(label = "beta div") +
+  scale_fill_viridis_c () +
+  theme_light()
+#ggforce::facet_wrap_paginate(~year_surv,
+# nrow = 2, ncol = 2, page = 2)
+
+beta.map
+
+# ALPHA
+
+effective.species <- fish.abun.clean.summary  %>%
   #calculate the Hill number for each stratum in each year
   group_by(stratum, year_surv)%>%
   dplyr::summarize(effective_species = exp(diversity(group_biomass, "shannon")))
