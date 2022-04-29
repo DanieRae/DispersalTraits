b8426d32b10515faa8d557e5ca691bf321246f6d
DanieRae <daniellembmacrae@gmail.com>
2022-04-29 15:14:34 -0400

    updates

--- 2.1-trait variation.R
+++ 2.1-trait variation.R
@@ -1,3 +1,45 @@
-plot(fish.traits.40NA$BodyShape)
-plot(fish.traits.40NA$DemersPelag)
-plot(fish.traits.40NA$LarvalStrategy)
+ggplot(data = fish.traits.40NA, aes(x= BodyShape))+
+  geom_bar()
+
+ggplot(data = fish.traits.40NA, aes(x= DemersPelag))+
+  geom_bar()
+
+ggplot(data = fish.traits.40NA, aes(x= LarvalStrategy))+
+  geom_bar()
+
+ggplot(data = fish.traits.40NA, aes(x= DepthRangeComDeep))+
+  geom_histogram()
+
+ggplot(data = fish.traits.40NA, aes(x= DepthRangeComShallow))+
+  geom_histogram()
+
+ggplot(data = fish.traits.40NA, aes(x= Length))+
+  geom_histogram()
+
+
+#Trying to make a stacked bar plot FAIL#
+
+fish.traits.40NA.rownames <- fish.traits.40NA
+taxa_name <- rownames(fish.traits.40NA)
+
+#fish.traits.40NA.rownames <- cbind(taxa_name,fish.traits.40NA.rownames)
+num <- unlist(lapply(fish.traits.40NA.rownames, is.numeric))
+cat <- unlist(lapply(fish.traits.40NA.rownames, is.factor))
+
+fish.traits.40NA.num <- fish.traits.40NA.rownames[,num]
+fish.traits.40NA.num<- cbind(taxa_name,fish.traits.40NA.num)
+
+fish.traits.40NA.cat <- fish.traits.40NA.rownames[,cat]
+fish.traits.40NA.cat<- cbind(taxa_name,fish.traits.40NA.cat)
+
+fish.traits.40.num.long <-fish.traits.40NA.num %>%
+  pivot_longer(!taxa_name, names_to = "Trait", values_to = "count")
+
+fish.traits.40.cat.long <-fish.traits.40NA.cat %>%
+  pivot_longer(!taxa_name, names_to = "Trait", values_to = "count")
+
+# Stacked + percent
+ggplot(fish.traits.40.cat.long, aes(fill=taxa_name, y=count, x=Trait)) + 
+  geom_bar(position="fill", stat="identity")
+
+
