d2d7d8f92d75b6ba98d50d54c553d8e8c451ee32
vlucet <valentin.lucet@gmail.com>
2023-12-18 15:21:32 -0500

    run FD, clean up correspondong script

--- analysis/paper/scripts/6-FD.R
+++ analysis/paper/scripts/6-FD.R
@@ -1,18 +1,17 @@
-### Running the functional diversity indices 
+### Running the functional diversity indices
 library(FD)
 
 ##ABUNDANCE DATA WRANGLING ----
 #abundance by site (strata,year)
 abun.fish.wide <-
-  as.data.table (spread(fish.abun.complete, taxa_name, group_biomass))
+  as.data.table(spread(fish.abun.complete, taxa_name, group_biomass))
 
 abun.fish.biomass <-
-  select(abun.fish.wide,-c ("stratum", "year_surv"))
+  select(abun.fish.wide,-c("stratum", "year_surv"))
 
 abun.fish.names <- sort(names(abun.fish.biomass), decreasing = FALSE)
 
 abun.fish.biomass <- abun.fish.biomass[, ..abun.fish.names]
-
 
 abun.fish.biomass <- as.matrix(abun.fish.biomass)
 
@@ -20,7 +19,9 @@ abun.fish.biomass <- as.matrix(abun.fish.biomass)
 
 functional.diversity <- dbFD(fish.traits.40NA, abun.fish.biomass, corr = "lingoes")
 
-##FD SAVED FILE ----
-saveRDS(functional.diversity, file = "FunctionalDiv.rds")
+  ##FD SAVED FILE ----
+saveRDS(functional.diversity,
+        file = here("analysis", "data", "derived_data", "FunctionalDiv.rds"))
 
-functional.diversity <- readRDS("FunctionalDiv.rds")
+functional.diversity <-
+  readRDS(here("analysis", "data", "derived_data", "FunctionalDiv.rds"))
